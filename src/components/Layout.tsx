c89d5753c4a3f1cf56de3dd97090e18b893651d1
Dominic German <dominicgerman@gmail.com>
2023-03-25 11:12:46 -0500

    Favicon, title, layout margin

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <div className="mx-6 md:mx-16 my-6 md:my-10 font-body leading-snug bg-main">
-      {children}
+      <div className="mx-auto max-w-screen-xl">{children}</div>
     </div>
   )
 }
